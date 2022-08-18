257254f508b55b54471cbf39b65a4df2401177d8
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-08-18 16:11:37 +0000

    create draw functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,21 @@ toggleBar.addEventListener("click", function() {
 
 /* Canvas- tutorial video (https://www.youtube.com/watch?v=Yvz_axxWG4Y&t=73s) */
 var canvas = document.getElementById("canvas1");
-var canvasSize = document.getElementById("test");
+var canvasSize = document.getElementById("size-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = canvasSize.clientWidth;
 canvas.height = canvasSize.clientHeight;
+
+drawBase();
+    drawPost();
+    drawTop();
+    drawRope();
+    drawHead();
+    drawBody();
+    drawArm1();
+    drawArm2();
+    drawLeg1();
+    drawLeg2();
 
 window.addEventListener("resize", function() {
     console.log(canvasSize.clientWidth);
@@ -20,43 +31,81 @@ window.addEventListener("resize", function() {
     canvas.width = canvasSize.clientWidth;
     canvas.height = canvasSize.clientHeight;
 
-    ctx.fillRect(canvas.width * 0.2, canvas.height * 0.9, canvas.width * 0.6, 5); //part 1
-    ctx.fillRect(canvas.width * 0.6, canvas.height * 0.1, 5, canvas.height * 0.8); //part 2
-    ctx.fillRect(canvas.width * 0.4, canvas.height * 0.1, canvas.width * 0.2, 5); //part 3
-    ctx.fillRect(canvas.width * 0.4, canvas.height * 0.1, 2, canvas.height * 0.2); //part 4
+    drawBase();
+    drawPost();
+    drawTop();
+    drawRope();
+    drawHead();
+    drawBody();
+    drawArm1();
+    drawArm2();
+    drawLeg1();
+    drawLeg2();
+});
 
-    
-    ctx.beginPath(); //part 5 - head   
+function drawBase() {
+    ctx.fillRect(canvas.width * 0.2, canvas.height * 0.9, canvas.width * 0.6, 5);
+};
+
+function drawPost() {
+    ctx.fillRect(canvas.width * 0.6, canvas.height * 0.1, 5, canvas.height * 0.8);
+};
+
+function drawTop() {
+    ctx.fillRect(canvas.width * 0.4, canvas.height * 0.1, canvas.width * 0.2, 5);
+};
+
+function drawRope() {
+    ctx.fillRect(canvas.width * 0.4, canvas.height * 0.1, 2, canvas.height * 0.2);
+};
+
+function drawHead() {
+    ctx.beginPath();
     ctx.arc(canvas.width * 0.4, canvas.height * 0.3, 10, 0, Math.PI * 2);
     ctx.fill();
+};
 
+function drawBody() {
     ctx.beginPath();
     ctx.moveTo(canvas.width * 0.4, canvas.height * 0.3);
-    ctx.lineTo(canvas.width * 0.4, canvas.height * 0.6); //part 6 - body 
-
-    ctx.moveTo(canvas.width * 0.4, canvas.height * 0.4);
-    ctx.lineTo(canvas.width * 0.5, canvas.height * 0.35); //part 7 - arm
-
-    ctx.moveTo(canvas.width * 0.4, canvas.height * 0.4);
-    ctx.lineTo(canvas.width * 0.3, canvas.height * 0.35); //part 8 - arm
-
-    ctx.moveTo(canvas.width * 0.4, canvas.height * 0.6);
-    ctx.lineTo(canvas.width * 0.45, canvas.height * 0.75); //part 9 - leg
-
-    ctx.moveTo(canvas.width * 0.4, canvas.height * 0.6);
-    ctx.lineTo(canvas.width * 0.35, canvas.height * 0.75); //part 10 - leg
-
+    ctx.lineTo(canvas.width * 0.4, canvas.height * 0.6);
     ctx.lineWidth = 5;
     ctx.stroke();
     ctx.closePath();
+};
 
+function drawArm1() {
+    ctx.beginPath();
+    ctx.moveTo(canvas.width * 0.4, canvas.height * 0.4);
+    ctx.lineTo(canvas.width * 0.5, canvas.height * 0.35);
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.closePath();
+};
 
+function drawArm2() {
+    ctx.beginPath();
+    ctx.moveTo(canvas.width * 0.4, canvas.height * 0.4);
+    ctx.lineTo(canvas.width * 0.3, canvas.height * 0.35); 
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.closePath();
+};
 
+function drawLeg1() {
+    ctx.beginPath();
+    ctx.moveTo(canvas.width * 0.4, canvas.height * 0.6);
+    ctx.lineTo(canvas.width * 0.45, canvas.height * 0.75) 
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.closePath();
+};
 
-    // ctx.fillRect(canvas.width * 0.4, canvas.height * 0.3, 2, canvas.height * 0.3); //part 5
-
-
-
-
-});
-
+function drawLeg2() {
+    ctx.beginPath();
+    ctx.moveTo(canvas.width * 0.4, canvas.height * 0.6);
+    ctx.lineTo(canvas.width * 0.35, canvas.height * 0.75);
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.closePath();
+};
